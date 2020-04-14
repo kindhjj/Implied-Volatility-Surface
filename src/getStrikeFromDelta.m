5c37d3f9fa9f81b14bf6ca361b09a1de915ea0bf
ShawnXiaoChao <43634393+ShawnXiaoChao@users.noreply.github.com>
2020-04-14 13:33:48 +0800

    Revised Q2.4

--- src/getStrikeFromDelta.m
+++ src/getStrikeFromDelta.m
@@ -10,12 +10,12 @@ function K = getStrikeFromDelta (fwd , T, cp , sigma , delta)
     getStrikeFromDeltaInputCheck(fwd , T, cp , sigma , delta,'Invalid Input');
     f = @(K) (OptionDelta(fwd,T,K,sigma,cp)-delta);
 %     [K,~] = secant(f,fwd/2,fwd*1.5,1e-6,4000);
-%     K = fsolve(f,fwd*0.1);
     
     try 
-%         [K,~] = bisection(f, fwd*0.001, fwd*1000, 1e-6);
-        [K,~] = secant(f,fwd/2,fwd*1.5,1e-6,4000);
-%          K = fsolve(f,fwd*0.1);
+%       K = bisection(f, fwd*0.001, fwd*1000, 1e-6);
+%         K = secant(f,fwd/2,fwd*1.5,1e-6,4000);
+%          K = fsolve(f,fwd-5);
+        K = zeroin(f,fwd*0.01,fwd*100);   
     catch
         disp('Fail to calculate strike price.Please Check')
     end
@@ -31,18 +31,17 @@ function delta = OptionDelta(fwd,T,K,sigma,cp)
     end
 end
 
-function [x1,h] = secant( f, x0, x1, xAcc, nIter )
+function x1 = secant( f, x0, x1, xAcc, nIter )
     
 	fx0 = f( x0 );
 
-	found = 0; h=[];
+	found = 0; 
 
 	for i = 1:nIter  % the limit on nIter is a guard for infinite loops
 		 x1Old = x1;
 		 fx1   = f( x1 );
 		 x1 = x1 - fx1 * ( x1  - x0 ) / ( fx1 - fx0 ); % update x1
-		 h(i,:)=[i, x1]; % unnecessary, just for display
-
+		
 		 if ( abs( x1 - x1Old ) < xAcc ) % exit criteria
 			found = 1;
 			break;
@@ -55,15 +54,14 @@ function [x1,h] = secant( f, x0, x1, xAcc, nIter )
 	if( ~found ), error( 'Maximum number of iterations exceeded ' ); end
 end
 
-function [c,x] = bisection( f, a, b, eps )
+function c = bisection( f, a, b, eps )
 	fa = f(a);
 	fb = f(b);
-	i  = 1; x = [];
 	if (fa*fb >= 0 ), error( 'f(a)f(b)>=0' ); end
 	while (b-a > eps)
 		c  = (a+b)/2;
 		fc = f(c);
-		x(i,:)=[i, c, a, b, b-a]; i=i+1;
+		
 		if (fc*fa<0)
 		   b = c;
 		   fb = fc;
@@ -72,6 +70,83 @@ function [c,x] = bisection( f, a, b, eps )
 		else
 		   a = c;
 		   fa = fc;
+        end
+    end
+end
+
+function b = zeroin(f,a,b)
+    fa = f(a);
+    fb = f(b);
+    if (fa*fb > 0 ), error( 'f(a)f(b)>=0' ); end
+    
+    c = a;
+    fc = fa;
+    d = b-c;
+    e = d;
+    while fb~=0
+        % Rearrange a,b and c to satisfy:
+        %   f(x) changes sign between a and b.
+        %   abs(f(b)<abs(f(a).
+        %   c = previous b. 
+        % The next point is decided by:
+        %   Bisection point, (a+b)/2.
+        %   Secant point determined by b and c.
+        %   Inverse quadratic interpolation point determined by a, b and c 
+        
+        if (fa*fb > 0 )
+            a = c; fa = fc;
+            d = b-c; e = d;
+        end
+        if abs(fa) < abs(fb)
+            c = b; b = a; a = c;
+            fc = fb; fb = fa; fa = fc;
+        end
+        
+        %judge if convergence
+        m = 0.5 * (a-b);
+        tol = 2.0 * eps * max(abs(b),1.0);
+        if (abs(m) <= tol) || (fb == 0.0)
+            break
+        end
+        
+        % choose next point among the three methods
+        if (abs(e) < tol || abs(fc) <= abs(fb))
+            %bisection
+            d = m;
+            e = m;
+        else
+            %Interpation
+            s = fb/fc;
+            if (a==c)
+                p = 2.0*m*s;
+                q = 1.0 - s;
+            else
+                q = fc/fa;
+                r = fb/fa;
+                p = s*(2.0*m*q*(q-r) - (b-c)*(r-1.0));
+                q = (q - 1.0)*(r - 1.0)*(s - 1.0);
+            end
+            if p > 0
+                q = -q;
+            else
+                p = -p;
+            end
+            if (2.0*p < 3.0*m*q - abs(tol*q))&&(p<abs(0.5*e*q))
+                e = d;
+                d = p/q;
+            else
+                d = m;
+                e = m;
+            end
+        end
+        
+        %next point
+        c = b;
+        fc = fb;
+        if abs(d) > tol
+            b = b+d;
+        else
+            b = b-sign(b-a)*tol;
         end
     end
 end
