d14bdb6b8bcc3d0ba19b0b8fe3651c30ecc558a2
liyun0048 <53899798+liyun0048@users.noreply.github.com>
2020-04-19 20:33:40 +0800

    Q2.7

--- src/getPdf.m
+++ src/getPdf.m
@@ -6,27 +6,27 @@ end
 if length(T)>1
     error('Error. length of T larger than 1.')
 end
-if T<0||T>volSurface.Ts(end)
+if T<0||T>volSurf.Ts(end)
     error('Error. Invalid T value.')
 end
 if any(Ks<0)
     error('Error. K smaller than zero.')
 end
 %% Transform the data from volatility to option price space.
-% After interpolation in (K, sigma) space, either via cubic splines, SABR
-% interpolation, or some other method, we obtain enough data points to
-% estimate the implied strike price density functions at each expiry time.
-% We begin by converting the (K, sigma) pairs into (K, C) (respectively,
-% (K, P)) space.
-% Define sample strike prices for the interpolation of pdf.
-[Vs, fwd] = getVol(volSurf, T, fineK);
-Cs = getBlackCall(fwd, T, Ks, Vs);
-%% Estimate the implied densities by approximating derivatives.
-% Approximate the first derivative, at each distinct expiry time.
-% We use the discrete approximation to the first derivative.
-dK = diff(Ks);
-d1 = diff(Cs) ./ dK;
-% Approximate the second derivatives.
-d2K = dK(2:end);
-pdf = diff(d1) ./ d2K;
+% We begin by converting the (K, sigma) pairs into (K, C) space.
+h = 0.0001;
+pdf = NaN(size(Ks));
+for k = 1:length(Ks)
+    fineK = [Ks(k)-h, Ks(k), Ks(k)+h]';
+    [Vs, fwd] = getVol(volSurf, T, fineK);
+    Cs = getBlackCall(fwd, T, fineK, Vs);
+    %% Estimate the implied densities by approximating derivatives.
+    % Approximate the first derivative, at each distinct expiry time.
+    % We use the discrete approximation to the first derivative.
+    dK = diff(fineK);
+    d1 = diff(Cs) ./ dK;
+    % Approximate the second derivatives.
+    d2K = dK(2:end);
+    pdf(k) = diff(d1) ./ d2K;
+end
 end
