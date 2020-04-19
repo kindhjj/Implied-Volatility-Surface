acd072bc569147dd9c955d21baeace150d2c4b52
liyun0048 <53899798+liyun0048@users.noreply.github.com>
2020-04-19 20:34:15 +0800

    Q2.8

--- src/getEuropean.m
+++ src/getEuropean.m
@@ -8,7 +8,7 @@
 % u : forward price of the option ( undiscounted price )
 function u = getEuropean ( volSurface, T, payoff, ints )
 % Check inputs.
-if ~all(isfield(volSurf,{'spots','fwdCurve','Ts','slopes','smiles'}))
+if ~all(isfield(volSurface,{'spots','fwdCurve','Ts','slopes','smiles'}))
     error('Error. volSurface input error: the struct is not complete.')
 end
 if length(T)>1
@@ -18,14 +18,20 @@ if T<0||T>volSurface.Ts(end)
     error('Error. Invalid T value.')
 end
 % Use Trapzoid rule to solve the integral.
-h = 0.05;
-if (nargin < 4) || isempty(ints)
-    x = 0 : h : 1000;
+h = 0.0005;
+if (nargin < 4) || isequal(ints, [0,+Inf])
+    x = 0.0005 : h : 10;
+    y = getPdf(volSurface, T, x) .* payoff(x);
+    u = h * trapz(y);
 else
-    x = min(ints) : h : max(ints);
+    x1 = 0.0005 : h : ints(2);
+    y1 = getPdf(volSurface, T, x1) .* payoff(x1);
+    u1 = h * trapz(y1);
+    x2 = ints(2) : h : 10;
+    y2 = getPdf(volSurface, T, x2) .* payoff(x2);
+    u2 = h * trapz(y2);
+    u = u1 + u2;
 end
-y = getPdf(volSurface, T, x) .* payoff(x);
-u = h * trapz(y);
 end
 
 
