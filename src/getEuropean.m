8aa2390b79263d239d764ac74a50ec7a15667ee7
ShawnXiaoChao <43634393+ShawnXiaoChao@users.noreply.github.com>
2020-04-21 22:36:03 +0800

    Updated Q2.8 using Matlab Integral Function

--- src/getEuropean.m
+++ src/getEuropean.m
@@ -7,6 +7,7 @@
 % Output :
 % u : forward price of the option ( undiscounted price )
 function u = getEuropean ( volSurface, T, payoff, ints )
+
 % Check inputs.
 if ~all(isfield(volSurface,{'spots','fwdCurve','Ts','slopes','smiles'}))
     error('Error. volSurface input error: the struct is not complete.')
@@ -17,21 +18,33 @@ end
 if T<0||T>volSurface.Ts(end)
     error('Error. Invalid T value.')
 end
-% Use Trapzoid rule to solve the integral.
-h = 0.0001;
+% % Use Trapzoid rule to solve the integral.
+% h = 0.0001;
+% if (nargin < 4) || isequal(ints, [0,+Inf])
+%     x = 0.0005 : h : 10;
+%     y = getPdf(volSurface, T, x) .* payoff(x);
+%     u = h * trapz(y);
+% else
+%     x1 = 0.0005 : h : ints(2);
+%     y1 = getPdf(volSurface, T, x1) .* payoff(x1);
+%     u1 = h * trapz(y1);
+%     x2 = ints(2) : h : 10;
+%     y2 = getPdf(volSurface, T, x2) .* payoff(x2);
+%     u2 = h * trapz(y2);
+%     u = u1 + u2;
+% end
+
+% main body
 if (nargin < 4) || isequal(ints, [0,+Inf])
-    x = 0.0005 : h : 10;
-    y = getPdf(volSurface, T, x) .* payoff(x);
-    u = h * trapz(y);
+     f = @(x)(getPdf(volSurface, T,x ).* payoff(x));
+     u = integral(@(x)f(x),0.005,Inf);
 else
-    x1 = 0.0005 : h : ints(2);
-    y1 = getPdf(volSurface, T, x1) .* payoff(x1);
-    u1 = h * trapz(y1);
-    x2 = ints(2) : h : 10;
-    y2 = getPdf(volSurface, T, x2) .* payoff(x2);
-    u2 = h * trapz(y2);
-    u = u1 + u2;
+     f = @(x)(getPdf(volSurface, T,x ).* payoff(x));
+     u1 = integral(@(x)f(x),max(ints(1),0.005),ints(2));
+     u2 = integral(@(x)f(x),ints(2),Inf);
+     u = u1 + u2;
 end
+    
 end
 
 
