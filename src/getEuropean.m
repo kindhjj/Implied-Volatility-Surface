2aa91d50354229044807948a79472c041999f415
liyun0048 <53899798+liyun0048@users.noreply.github.com>
2020-04-22 16:19:25 +0800

    Add files via upload

--- src/getEuropean.m
+++ src/getEuropean.m
@@ -7,44 +7,38 @@
 % Output :
 % u : forward price of the option ( undiscounted price )
 function u = getEuropean ( volSurface, T, payoff, ints )
-
 % Check inputs.
 if ~all(isfield(volSurface,{'spots','fwdCurve','Ts','slopes','smiles'}))
     error('Error. volSurface input error: the struct is not complete.')
 end
 if length(T)>1
-    error('Error. length of T larger than 1.')
+    error('Error. Length of T larger than 1.')
 end
 if T<0||T>volSurface.Ts(end)
     error('Error. Invalid T value.')
 end
-% % Use Trapzoid rule to solve the integral.
-% h = 0.0001;
-% if (nargin < 4) || isequal(ints, [0,+Inf])
-%     x = 0.0005 : h : 10;
-%     y = getPdf(volSurface, T, x) .* payoff(x);
-%     u = h * trapz(y);
-% else
-%     x1 = 0.0005 : h : ints(2);
-%     y1 = getPdf(volSurface, T, x1) .* payoff(x1);
-%     u1 = h * trapz(y1);
-%     x2 = ints(2) : h : 10;
-%     y2 = getPdf(volSurface, T, x2) .* payoff(x2);
-%     u2 = h * trapz(y2);
-%     u = u1 + u2;
-% end
+if isa(payoff,'function_handle')
+    error('Error. Invalid payoff function.')
+end
+if ~isempty(ints)
+    if length(ints) < 2
+        error('Error. The number of elements in integration interval is smaller than 2.')
+    end
+    if ~all(ints >= 0)
+        error('Error. Invalid integration interval.')
+    end
+end
 
 % main body
 if (nargin < 4) || isequal(ints, [0,+Inf])
-     f = @(x)(getPdf(volSurface, T,x ).* payoff(x));
-     u = integral(@(x)f(x),0.005,Inf);
+     f = @(x) (getPdf(volSurface, T, x) .* payoff(x));
+     u = integral(@(x) f(x), 0.005, Inf);
 else
-     f = @(x)(getPdf(volSurface, T,x ).* payoff(x));
-     u1 = integral(@(x)f(x),max(ints(1),0.005),ints(2));
-     u2 = integral(@(x)f(x),ints(2),Inf);
+     f = @(x)(getPdf(volSurface, T, x) .* payoff(x));
+     u1 = integral(@(x) f(x), max(ints(1), 0.005), ints(2));
+     u2 = integral(@(x) f(x), ints(2), Inf);
      u = u1 + u2;
 end
-    
 end
 
 
