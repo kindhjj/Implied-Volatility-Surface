51c13dcb770e20a80d23e2d231c08654db7a684c
liyun0048 <53899798+liyun0048@users.noreply.github.com>
2020-04-23 15:17:56 +0800

    Add files via upload

--- src/getPdf.m
+++ src/getPdf.m
@@ -22,13 +22,17 @@ dK = [h; h];
 
 for k = 1:length(Ks)
     fineK = fineK_matrix(:,k);
-    [Vs, fwd] = getVol(volSurf, T, fineK);
-    Cs = getBlackCall(fwd, T, fineK, Vs);
-    % Estimate the implied densities by approximating derivatives.
-    % Approximate the first derivative, at each distinct expiry time.
-    % We use the discrete approximation to the first derivative.
-    d1 = diff(Cs) ./ dK;
-    % Approximate the second derivatives.
-    pdf(k) = diff(d1) / h;
+    if fineK(1) < 0
+        pdf(k) = 0;
+    else
+        [Vs, fwd] = getVol(volSurf, T, fineK);
+        Cs = getBlackCall(fwd, T, fineK, Vs);
+        % Estimate the implied densities by approximating derivatives.
+        % Approximate the first derivative, at each distinct expiry time.
+        % We use the discrete approximation to the first derivative.
+        d1 = diff(Cs) ./ dK;
+        % Approximate the second derivatives.
+        pdf(k) = diff(d1) / h;
+    end
 end
 end
