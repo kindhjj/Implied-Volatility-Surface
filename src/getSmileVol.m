91ca9f92a8e2de67a68b1bd54d686925e3263c6b
kindhjj <kindhjj@gmail.com>
2020-04-23 22:30:12 +0800

    Q2.5

--- src/getSmileVol.m
+++ src/getSmileVol.m
@@ -20,6 +20,7 @@ function vols = getSmileVol(curve, Ks)
     inter = Ks((Ks >= K1) & (Ks <= KN));
     right = Ks(Ks > KN);
     
+    %interpolation and extrapolation
     vol_left = ppval(curve.pp, K1) + ...
         curve.AL * tanh(curve.BL * (K1 - left));
     vol_inter = ppval(curve.pp, inter);
